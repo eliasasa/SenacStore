3804b757a12458212fb7efeb41fb8668826db68a
eliasasa <eliasnetoneto1@hotmail.com>
2025-02-18 10:14:52 -0400

    Userpage 75%

--- src/js/userPage.js
+++ src/js/userPage.js
@@ -1,15 +1,16 @@
-import { getInfoUser } from "../components/infoUser.js";
+import { getInfoUser } from "./infoUser.js";
 import { renderUserInfo } from "../components/renderUser.js";
 import { mostrarPopup, fecharPopup } from "../components/gerar_popUp.js";
 
 async function userInfo(event) {
     event.preventDefault();
-    const id = parseInt(document.getElementById('idUser').value)
+    const id = 1;
+    // const id = parseInt(document.getElementById('idUser').value)
     try {
         const resposta = await getInfoUser(id);
         if (resposta) {
             console.log(resposta);
-            renderUserInfo(resposta);
+            renderUserInfo(document.getElementById('user_info'), resposta);
         } else {
             // document.getElementById('popup-overlay').addEventListener('click', redirecionarParaLogin);
             // document.getElementById('btn-fechar-popup').addEventListener('click', redirecionarParaLogin);
@@ -26,5 +27,7 @@ async function userInfo(event) {
 function redirecionarParaLogin() {
     window.location.href = 'index.html';
 }
+
+document.addEventListener('DOMContentLoaded', userInfo);
 
 document.getElementById('teste').addEventListener('click', userInfo);
