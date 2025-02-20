20a0a547252ada092c4ac3b862b50c02132ff228
Rafaela Vergotti <rafavergottii@gmail.com>
2025-02-20 09:00:37 -0400

    89%finalizado

--- src/js/home/favoritos.js
+++ src/js/home/favoritos.js
@@ -1,47 +1,27 @@
 import {criar_filtro} from "./filtro_home.js";
-import { addAoFavoritos } from '../produto.js';
+import componente_produto from "./produto_home.js"
 
-const ids = [1, 2, 3, 4, 5,6,7,8,9,10];
-console.log(localStorage.getItem('favoritos'))
-// window.onload = () => {
-//   fetchProdutos();
-//   carregarCategorias();
-// }
+const favoritados = JSON.parse(localStorage.getItem('favoritos'));
+console.log(favoritados)
 
-// async function fetchProdutos() {
-//   for (let id of ids) {
-//     try {
-//       const resposta = await fetch(`https://fakestoreapi.com/products/${id}`);
-//       const produto = await resposta.json();
-//       console.log(produto);
+favoritados.map((itemAtual)=>{
+  const produtoDiv = `<div class='produto-div'> 
+      <img src='${itemAtual.image}'class="imagem-produto">
+      <h2>${itemAtual.title}</h2> <span>R$${itemAtual.price}</span>
+      </div>`
 
-//       const produtoDiv = `<div class='produto-div'> 
-//       <img src='${produto.image}'class="imagem-produto">
-//       <h2>${produto.title}</h2> <span>R$${produto.price}</span>
-//       </div>`
+  document.getElementById('listaFavoritos').innerHTML += produtoDiv;
+})
+
+function carregarCategorias() {
+  fetch('https://fakestoreapi.com/products/categories')
+    .then(res => res.json())
+    .then(categorias => {
+      document.getElementById('filtroCategorias').innerHTML = criar_filtro(categorias);
       
-    
-      
-//       document.getElementById('listaFavoritos').innerHTML += produtoDiv;
-
-
-
-      
-//     } catch (error) {
-//       console.error(`Erro ${id}:`, error);
-//     }
-//   }
-// }
-
-// function carregarCategorias() {
-//   fetch('https://fakestoreapi.com/products/categories')
-//     .then(res => res.json())
-//     .then(categorias => {
-//       document.getElementById('filtroCategorias').innerHTML = criar_filtro(categorias);
-      
-//       document.querySelector('#filtro_produtos select').addEventListener('change', (event) => {
-//         const categoriaSelecionada = event.target.value;
-//         exibirFavoritos(categoriaSelecionada);
-//       });
-//     });
-// }
+      document.querySelector('#filtro_produtos select').addEventListener('change', (event) => {
+        const categoriaSelecionada = event.target.value;
+        exibirFavoritos(categoriaSelecionada);
+      });
+    });
+}
