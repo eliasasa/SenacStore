5e05a6857088c323e9c6acdc08f4d7cacd3bb584
Ricardo <sousasilvaricardo9@gmail.com>
2025-02-20 08:16:42 -0400

    salvando

--- src/js/login.js
+++ src/js/login.js
@@ -34,7 +34,7 @@ async function fetchLogin(usuario, senha) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            username: usuario,
+            username: usuario, 
             password: senha
         })
     });
